151a63a0b24a3761ebf84c0b4affa9db41ea0893
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:18:01 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.5.x branch

--- src/main/java/com/arcsoft/face/spring/boot/ArcFaceMessageSource.java
+++ src/main/java/com/arcsoft/face/spring/boot/ArcFaceMessageSource.java
@@ -20,7 +20,7 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 
 /**
  * TODO
- * @author 		： <a href="https://github.com/hiwepy">wandl</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class ArcFaceMessageSource extends ResourceBundleMessageSource {
 	
